fb2cacbc499679242e22b61db96b7d3f0ea8d6e2
saulo <saulomiqueias7@gmail.com>
2021-10-18 13:39:13 -0300

    model refactor

--- lib/src/app/modules/user_repo/data/models/user_repo_model.dart
+++ lib/src/app/modules/user_repo/data/models/user_repo_model.dart
@@ -2,13 +2,15 @@ import 'package:equatable/equatable.dart';
 
 class UserRepoModel extends Equatable {
   final int id;
-  final String name;
-  final String language;
+  final String? name;
+  final String? language;
+  final String? description;
   final String createdAt;
   final int stargazersCount;
   final int watchersCount;
 
   const UserRepoModel({
+    required this.description,
     required this.id,
     required this.name,
     required this.language,
@@ -20,11 +22,12 @@ class UserRepoModel extends Equatable {
   factory UserRepoModel.fromJson(Map<String, dynamic> json) {
     return UserRepoModel(
       id: json['id'],
-      name: json['name'],
-      language: json['language'],
+      name: json['name'] ?? "Não informado",
+      language: json['language'] ?? "Não informado",
       stargazersCount: json['stargazers_count'],
       createdAt: json['created_at'],
       watchersCount: json['watchers_count'],
+      description: json['description'] ?? "Não informado",
     );
   }
 
